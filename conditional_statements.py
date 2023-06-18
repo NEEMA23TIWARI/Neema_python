53ee4283dd5f6b79a73d0b7aea1dcc327d17a8a9
NEEMA23TIWARI <136810077+NEEMA23TIWARI@users.noreply.github.com>
2023-06-18 09:54:12 +0530

    Add files via upload

---
+++ conditional_statements.py
@@ -0,0 +1,47 @@
+'''#conditional statements
+
+print("hello")
+pass  #empty statement
+
+#syntax
+
+if(condition):
+    #statements
+    else:
+        #statements'''
+
+
+#wap to input a number and check whether it is positive or not
+
+
+num=int(input("enter a number"))
+if(num>0):
+   print("positive")
+else:
+          print("negative")
+
+
+
+ #wap to input a number and check whether it is divisible by both 5 and 3 using if else
+
+num=int(input("enter any number"))
+if(num%3==0 and num%5==0):
+                  print("no.is divisible by both 3 and 5")
+else:
+                  print("not divisible")'''
+
+num=int(input("enter a number"))
+if(num>0):
+   print("positive")
+else:
+          print("negative")
+
+'''#wap to input a character and check whether it is a vowel or consonant
+
+ch=input("enter any letter")
+if(ch=='a' or ch=='e'or ch=='i'or ch=='o'or ch=='u'):
+     print("it is a vowel")
+else:
+         print("it is a consonant")'''
+ 
+                  
