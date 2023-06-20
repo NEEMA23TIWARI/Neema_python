1d159e8e18cacda7998f192cd10b5adaad89b92c
NEEMA23TIWARI <136810077+NEEMA23TIWARI@users.noreply.github.com>
2023-06-20 10:29:04 +0530

    Add files via upload

---
+++ function program.py
@@ -0,0 +1,17 @@
+#wap to create a function which should accept two parameters as input and swap them and print updated vaslues
+'''def swap (x,y):
+    temp=x
+    x=y
+    y=temp
+    print(x,y)
+a=input("enter any no")
+b=input("enter the value")
+swap(a,b)'''
+
+#or
+def demo(a,b):
+    a,b=b,a
+    print(a,b)
+p=input("enter value")
+q=input("enter value")
+demo(p,q)     
---
+++ function.py
@@ -0,0 +1,29 @@
+#functions
+'''def demo():        #user defined function
+    print("welcome")
+demo()        #calling of function
+
+def demo(name):
+    print(name)
+demo("john")'''
+
+'''def demo (name):      #formal parameter
+    print(name)
+n=input("enter name")
+demo(n)     #actual parameter'''
+
+'''#function can return any value
+def demo():
+    return 2
+print(demo())  #or result=demo()
+                #print(result)'''
+
+#a function can return 2 values
+
+def demo():
+    return 2,8
+n1,n2=demo()
+print(n1,n2)
+ 
+
+
