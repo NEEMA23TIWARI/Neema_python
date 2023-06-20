098d83e6c0b56c5acfa52cf6734c3574d7aec083
NEEMA23TIWARI <136810077+NEEMA23TIWARI@users.noreply.github.com>
2023-06-20 11:14:47 +0530

    Add files via upload

---
+++ tupples.py
@@ -0,0 +1,13 @@
+#TUPPLES
+t=(1,2,3,4,"hello")          #representation of tupples(to store confidential data we use tupple)
+t2=tuple((1,2,3,"hello"))    #representation of tupples(so that no chnages can be made)
+print(t)
+print(t2)
+a=(1,2,3,4)
+print(max(a))          #to give maximum no.
+print(min(a))         #to give minimum no.
+
+#TUPPLE UNPACKING
+a,b,c,d,e=(1,2,3,"hello","bye")
+print(a,b,c,d,e)
+
